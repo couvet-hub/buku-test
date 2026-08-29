b4ddbe360a9b000e503eda491d9d57f263fc6a87
couvet-hub <couvet31@gmail.com>
2026-08-29 21:56:20 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
 from transformers import pipeline
 import torch
 
-print("LOAD MODEL...")
 dewa = pipeline("text-generation", model="./dewa_mtk_300mb", dtype=torch.float16, device_map="cpu")
 
-pertanyaan = "Apa rumus luas persegi kelas 3 SD?"
+pertanyaan = "Kelas 1: Berapa hasil dari 2 + 4?"
 messages = [{"role": "user", "content": pertanyaan}]
 prompt = dewa.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
 
-print("JAWABAN:")
-hasil = dewa(prompt, max_new_tokens=150)
+hasil = dewa(prompt, max_new_tokens=100)
 print(hasil[0]['generated_text'])
